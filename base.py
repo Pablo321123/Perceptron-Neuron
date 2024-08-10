16d3abb200b7e632bf404c37d1fae41375af6b31
Pablo Vasconcelos <pablo_vasconcelos123@hotmail.com>
2024-08-09 23:33:01 -0300

    Database added and data conveted

--- base.py
+++ base.py
@@ -1,5 +1,6 @@
 import math
 import numpy as np
+import pandas as pd
 
 
 class Perceptron:
@@ -23,57 +24,63 @@ class Perceptron:
         self.max_it = max_it + 1  # Maximo de Épocas
         self.fator_aprendizagem = fator_aprendizagem  # Fator de aprendizagem
         self.ve = [[] for len in range(n)]  # vetor de erros
+        
+        print(D)
 
     def degrauFunction(self, value, j):
         return 1 if value[j] >= 0 else 0
 
     def start_training(self):
+
         while self.t < self.max_it and self.Error > 0:
             self.Error = 0
             x = self.X
-            for i in range(self.n):  # neuronio
-                d = self.D[i]
-                for j in range(self.X.shape[1]):  # Numero de colunas (entradas)
-
+            for j in range(self.X.shape[1]):  # Numero de colunas (entradas)
+                Y = [[] for len in range(self.n)]
+                for i in range(self.n):  # neuronio
+                    d = self.D[i]
                     w = self.W[i]
                     b = self.b[i]
 
-                    y = self.degrauFunction(
-                        (w @ x) + b, j
-                    )  # @ -> Operador de produto escalar entre matrizes
+                    # @ -> Operador de produto escalar entre matrizes
+                    y_i = self.degrauFunction((w @ x) + b, j)
+                    Y[i].append(y_i)
 
-                    e = d[j] - y
-
-                    self.W[i] = (
-                        w + (self.fator_aprendizagem * e) * x.T[j]
-                    )  # -> transposta
-                    self.b[i] = b + self.fator_aprendizagem * e * 1  # -> X[0] = 1
-                    self.Error = self.Error + math.pow(e, 2)  # sum(math.pow(e, 2))
-
-                    self.ve[i].append(e)
-                    print(w)
-                    print(self.b)
+                e = (d[j] - y_i)  # Fazer as verificações para verificar se as colunas são iguais
+                self.W[i] = w + (self.fator_aprendizagem * e) * x.T[j]  # -> transposta
+                self.b[i] = b + self.fator_aprendizagem * e * 1  # -> X[0] = 1
+                self.Error = self.Error + math.pow(e, 2)  # sum(math.pow(e, 2))
+                self.ve[i].append(e)
+                # print(w)
+                # print(self.b)
 
             self.t = self.t + 1
 
     def start_test(self, X, D):
         acertos = 0
         taxa_acertos = 0
+        Y = [[] for len in range(self.n)]
         ve_teste = [[] for len in range(self.n)]
 
         for i in range(self.n):  # neuronio
             d = D[i]
             for j in range(self.X.shape[1]):
-                y = self.degrauFunction((np.dot(self.W[i], X)) + self.b[i], j)
-                e = d[j] - y
+                y_i = self.degrauFunction((np.dot(self.W[i], X)) + self.b[i], j)
+                Y[i].append(y_i)
+                e = d[j] - y_i
                 ve_teste[i].append(e)
-
                 if e == 0:
                     acertos = acertos + 1
+
         taxa_acertos = (acertos / self.X.shape[1]) * 100
         print(f"Acuracia: {taxa_acertos}%")
 
 
-p = Perceptron(4, 0.1, [[0, 0, 1, 1], [0, 1, 0, 1]], [[0, 1, 1, 1]], 1)
-p.start_training()
-p.start_test([[0, 0, 1, 1], [1, 0, 1, 0]], [[1, 0, 1, 1]])
+def loadData(fileName):
+    glass_list = pd.read_csv(f"../Perceptron-Neuron/data/{fileName}")
+    return glass_list
+
+
+# p = Perceptron(4, 0.1, [[0, 0, 1, 1], [0, 1, 0, 1]], [[0, 1, 1, 1]], 1)
+# p.start_training()
+# p.start_test([[0, 0, 1, 1], [1, 0, 1, 0]], [[1, 0, 1, 1]])
