a247e68b4db0dda9614a0514f97287fd9702fc6a
Pablo Vasconcelos <pablo_vasconcelos123@hotmail.com>
2024-08-11 23:59:37 -0300

    Done!
    Fix a bug with normalize values for X
    
    Fix a bug with overflow error
    
    Plot graphic implemmented!

--- base.py
+++ base.py
@@ -25,11 +25,10 @@ class Perceptron:
         self.fator_aprendizagem = fator_aprendizagem  # Fator de aprendizagem
         self.ve = [[] for len in range(self.X.shape[1])]  # vetor de erros
 
-    def degrauFunction(self, value, j):
-        return 1 if value[j] >= 0 else 0
+        self.sigmoid = lambda x, j: 1 / (np.exp(-x[j]))
+        self.degrau = lambda value, j: 1 if value[j] >= 0 else 0
 
-    def start_training(self):
-
+    def start_training(self, sigmoid=False):
         while self.t < self.max_it and self.Error > 0:
             self.Error = 0
             x = self.X
@@ -42,45 +41,70 @@ class Perceptron:
                     b = self.b[i]
 
                     # @ -> Operador de produto escalar entre matrizes
-                    y_i = self.degrauFunction((w @ x) + b, j)
+                    y_i = (
+                        self.degrau((w @ x) + b, j)
+                        if not sigmoid
+                        else self.sigmoid((w @ x) + b, j)
+                    )
                     Y[i] = y_i  # Vetor de saida de todos os neuronios
 
-                e = (d - Y)  # Fazer as verificações para verificar se as colunas são iguais     
+                if sigmoid:
+                    max_y = max(Y)
+                    Y = [1 if x == max_y else 0 for x in Y]
+
+                # Fazer as verificações para verificar se as colunas são iguais
+                e = d - Y
                 for i in range(self.n):
-                    self.W[i] = self.W[i] + (self.fator_aprendizagem * e[i]) * x.T[j]  # -> transposta
-                    self.b[i] = self.b[i] + (self.fator_aprendizagem * e[i] * 1)  # -> X[0] = 1
+                    x_t = x.T[j]
+                    self.W[i] = (
+                        self.W[i] + (self.fator_aprendizagem * e[i]) * x_t
+                    )  # -> transposta
+                    self.b[i] = self.b[i] + (
+                        self.fator_aprendizagem * e[i] * 1
+                    )  # -> X[0] = 1
                     self.Error = self.Error + math.pow(e[i], 2)  # sum(math.pow(e, 2))
                 self.ve[j].append(e)
-                #print(Y)
-
+                # print(Y)
 
             self.t = self.t + 1
-        
-        print(self.W)
-        print(self.b)
-        
-            
-        
 
-    def start_test(self, X, D):
+        print("Pesos:\n")
+        print(pd.DataFrame(self.W))
+        print("\nBias:\n")
+        print(pd.DataFrame(self.b))
+
+    def start_test(self, X_teste, D_teste, sigmoid=False):
         acertos = 0
         taxa_acertos = 0
-        Y = [[] for len in range(self.n)]
         ve_teste = [[] for len in range(self.n)]
-        X = np.array(X)
+        X = np.array(X_teste)
 
-        for i in range(self.n):  # neuronio
-            d = D[i]
-            for j in range(X.shape[1]):
-                y_i = self.degrauFunction((np.dot(self.W[i], X)) + self.b[i], j)
-                Y[i].append(y_i)
-                e = d[j] - y_i
-                ve_teste[i].append(e)
-                if e == 0:
-                    acertos = acertos + 1
+        for j in range(X.shape[1]):
+            Y = []
+            d = np.array(D_teste[j])
+            for i in range(self.n):  # neuronio
+                Y.append([])
+                y_i = (
+                    self.degrau((self.W[i] @ X) + self.b[i], j)
+                    if not sigmoid
+                    else self.sigmoid((self.W[i] @ X) + self.b[i], j)
+                )  # W[i] peso já treinado do neuronio i
+                Y[i] = y_i
+
+            if sigmoid:
+                max_y = max(Y)
+                Y = [1 if x == max_y else 0 for x in Y]
+
+            e = d - Y
+            ve_teste[i].append(e)
+
+            if max(e) == 0 and min(e) == 0:
+                acertos = acertos + 1
 
         taxa_acertos = (acertos / X.shape[1]) * 100
         print(f"Acuracia: {taxa_acertos}%")
+        
+        return taxa_acertos
 
 
 def loadData(fileName):
